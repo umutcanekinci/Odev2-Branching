226ebbbbd2ddc0a313803152361b90721b8d39db
umutcanekinci <umutcannekinci@gmail.com>
2025-04-19 16:27:38 +0300

    __main__.py file has added.

---
+++ __main__.py
@@ -0,0 +1,28 @@
+import pygame
+
+WHITE = (255, 255, 255)
+
+def main():
+    window = initWindow()
+    start(window)
+
+def initWindow():
+    pygame.init()
+    window = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Git - Branching")
+    return window
+
+def start(window):
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        window.fill(WHITE)
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
